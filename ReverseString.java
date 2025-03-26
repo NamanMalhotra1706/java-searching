bce85430cea3ebacda9c0f0a0bbc4d705824e348
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-26 10:01:52 +0530

    [Naman Malhotra] ADD: Reverse a String Using StringBuilder

---
+++ ReverseString.java
@@ -0,0 +1,8 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("hello");
+        str.reverse();
+        System.out.println(str);
+    }
+}
+
