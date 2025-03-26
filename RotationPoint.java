2654d9fd6e370b0dd8c8ddacb833e8f0b67d59bf
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-27 00:42:54 +0530

    [Naman Malhotra] ADD: Find the Rotation Point in a Rotated Sorted Array

---
+++ RotationPoint.java
@@ -0,0 +1,21 @@
+public class RotationPoint {
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+
+        int rotationIndex = findRotationPoint(nums);
+        System.out.println("Rotation point index: " + rotationIndex);
+    }
+
+    public static int findRotationPoint(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
