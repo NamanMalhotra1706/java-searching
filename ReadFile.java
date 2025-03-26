bf2ca3004161afe49e92a5cdf72f2a7f717b3839
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-26 10:25:27 +0530

    [Naman Malhotra] ADD: Read a File Line by Line Using FileReader

---
+++ ReadFile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        String fileName = "SampleFile"; // Change this to the path of your file
+
+        try (FileReader fileReader = new FileReader(fileName);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
